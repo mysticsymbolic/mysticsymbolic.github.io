e507f6311dddb1b712f7b825026e3eabd265f7f1
Atul Varma <varmaa@gmail.com>
2021-02-02 19:40:56 -0500

    remove fill, stroke attrs

--- lib/vocabulary.ts
+++ lib/vocabulary.ts
@@ -5,14 +5,25 @@ import cheerio from "cheerio";
 const MY_DIR = __dirname;
 const SVG_DIR = path.join(MY_DIR, "..", "svg");
 
+function removeAttr(attr: string, $: cheerio.Root, g: cheerio.Cheerio) {
+  const items = g.find(`[${attr}]`);
+  items.each(function (this: any, i, el) {
+    $((this)).removeAttr(attr);
+  });
+}
+
 export function build() {
   const filenames = fs.readdirSync(SVG_DIR);
   for (let filename of filenames) {
     if (path.extname(filename) === ".svg") {
-      const svg = fs.readFileSync(path.join(SVG_DIR, filename), {
+      const svgMarkup = fs.readFileSync(path.join(SVG_DIR, filename), {
         encoding: "utf-8",
       });
-      console.log(filename, svg);
+      const $ = cheerio.load(svgMarkup);
+      const g = $("svg > g");
+      removeAttr('fill', $, g);
+      removeAttr('stroke', $, g);
+      console.log(filename, g.html());
     }
   }
 }
