327f99fdae0d5ae9db0c4f807e04ded776b6f630
Atul Varma <varmaa@gmail.com>
2021-02-27 08:35:34 -0500

    Factor out auto-sizing-svg.tsx.

--- lib/pages/creature-page.tsx
+++ lib/pages/creature-page.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
+import React, { useContext, useRef, useState } from "react";
 import { SvgVocabulary } from "../svg-vocabulary";
 import {
   createSvgSymbolContext,
@@ -18,6 +18,7 @@ import { Random } from "../random";
 import { SymbolContextWidget } from "../symbol-context-widget";
 import { range } from "../util";
 import { getBoundingBoxCenter, uniformlyScaleToFit } from "../bounding-box";
+import { AutoSizingSvg } from "../auto-sizing-svg";
 
 const DEFAULT_BG_COLOR = "#858585";
 
@@ -438,51 +439,6 @@ function exportSvg(filename: string, svgRef: React.RefObject<SVGSVGElement>) {
   anchor.click();
   document.body.removeChild(anchor);
 }
-
-const AutoSizingSvg = React.forwardRef(
-  (
-    props: {
-      padding: number;
-      bgColor?: string;
-      children: JSX.Element | JSX.Element[];
-    },
-    ref: React.ForwardedRef<SVGSVGElement>
-  ) => {
-    const { bgColor, padding } = props;
-    const [x, setX] = useState(0);
-    const [y, setY] = useState(0);
-    const [width, setWidth] = useState(1);
-    const [height, setHeight] = useState(1);
-    const gRef = useRef<SVGGElement>(null);
-
-    useEffect(() => {
-      const svgEl = gRef.current;
-      if (svgEl) {
-        const bbox = svgEl.getBBox();
-        setX(bbox.x - padding);
-        setY(bbox.y - padding);
-        setWidth(bbox.width + padding * 2);
-        setHeight(bbox.height + padding * 2);
-      }
-    });
-
-    return (
-      <svg
-        version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-        width={`${width}px`}
-        height={`${height}px`}
-        viewBox={`${x} ${y} ${width} ${height}`}
-        ref={ref}
-      >
-        {bgColor && (
-          <rect x={x} y={y} width={width} height={height} fill={bgColor} />
-        )}
-        <g ref={gRef}>{props.children}</g>
-      </svg>
-    );
-  }
-);
 
 function getDownloadFilename(randomSeed: number | null) {
   let downloadBasename = "mystic-symbolic-creature";
