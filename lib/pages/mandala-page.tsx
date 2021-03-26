430bd5268e94d1970bcfdac3ae7857a9845bd5fd
Atul Varma <varmaa@gmail.com>
2021-03-26 16:11:52 -0400

    Put an eye on it.

--- lib/pages/mandala-page.tsx
+++ lib/pages/mandala-page.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { AutoSizingSvg } from "../auto-sizing-svg";
+import { createCreatureSymbolFactory } from "../creature-symbol-factory";
+import { getSvgSymbol } from "../svg-vocabulary";
+
+const symbol = createCreatureSymbolFactory(getSvgSymbol);
+
+const Eye = symbol("eye");
 
 export const MandalaPage: React.FC<{}> = () => {
   return (
     <>
       <h1>Mandala!</h1>
+      <AutoSizingSvg padding={20}>
+        <Eye />
+      </AutoSizingSvg>
     </>
   );
 };
--- lib/svg-vocabulary.ts
+++ lib/svg-vocabulary.ts
@@ -2,3 +2,22 @@ import type { SvgSymbolData } from "./svg-symbol";
 import _SvgVocabulary from "./_svg-vocabulary.json";
 
 export const SvgVocabulary: SvgSymbolData[] = _SvgVocabulary as any;
+
+/**
+ * Mapping from symbol names to symbol data, for quick and easy access.
+ */
+const SYMBOL_MAP = new Map(
+  SvgVocabulary.map((symbol) => [symbol.name, symbol])
+);
+
+/**
+ * Returns the data for the given symbol, throwing an error
+ * if it doesn't exist.
+ */
+export function getSvgSymbol(name: string): SvgSymbolData {
+  const symbol = SYMBOL_MAP.get(name);
+  if (!symbol) {
+    throw new Error(`Unable to find the symbol "${name}"!`);
+  }
+  return symbol;
+}
