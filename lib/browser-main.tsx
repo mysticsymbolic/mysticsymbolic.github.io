b5dfaf945c38942d63dafa7d296ba4938f833dbe
Atul Varma <varmaa@gmail.com>
2021-04-02 17:29:40 -0400

    Factor out pages/index.tsx.

--- lib/browser-main.tsx
+++ lib/browser-main.tsx
@@ -1,22 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { WavesPage } from "./pages/waves-page";
-import { VocabularyPage } from "./pages/vocabulary-page";
-import { CreaturePage } from "./pages/creature-page";
-import { MandalaPage } from "./pages/mandala-page";
-import { DebugPage } from "./pages/debug-page";
-
-const Pages = {
-  vocabulary: VocabularyPage,
-  creature: CreaturePage,
-  waves: WavesPage,
-  mandala: MandalaPage,
-  debug: DebugPage,
-};
-
-type PageName = keyof typeof Pages;
-
-const pageNames = Object.keys(Pages) as PageName[];
+import { pageNames, Pages, toPageName, DEFAULT_PAGE } from "./pages";
 
 const APP_ID = "app";
 
@@ -42,7 +26,7 @@ const Navbar: React.FC<{ currPageName: string }> = (props) => (
 
 const App: React.FC<{}> = (props) => {
   const page = new URLSearchParams(window.location.search);
-  const currPageName = toPageName(page.get("p") || "", "vocabulary");
+  const currPageName = toPageName(page.get("p") || "", DEFAULT_PAGE);
   const PageComponent = Pages[currPageName];
 
   return (
@@ -67,12 +51,3 @@ const App: React.FC<{}> = (props) => {
 };
 
 ReactDOM.render(<App />, appEl);
-
-function isPageName(page: string): page is PageName {
-  return pageNames.includes(page as any);
-}
-
-function toPageName(page: string, defaultValue: PageName): PageName {
-  if (isPageName(page)) return page;
-  return defaultValue;
-}
