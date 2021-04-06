c4c85066bf7c4a6941d19cf653ee05681978bf68
Maneesh Yadav <mittimithai@gmail.com>
2021-04-06 14:33:35 -0700

    Random sampling from CIELUV now via colorspaces, fallback to RGB.

--- lib/random-colors.ts
+++ lib/random-colors.ts
@@ -1,6 +1,6 @@
 import { Random } from "./random";
 import { range } from "./util";
-import { hsluvToHex } from "hsluv";
+import * as colorspaces from 'colorspaces';
 
 /**
  * Clamp the given number to be between 0 and 255, then
@@ -25,13 +25,50 @@ function createRandomColor(rng: Random): string {
   return "#" + rgb.map(clampedByteToHex).join("");
 }
 */
-
+/*
 function createRandomColor(rng: Random): string {
     let hue: number = rng.inRange({ min: 0, max: 360, step: 1 });
     let saturation: number = rng.inRange({ min: 0, max: 100, step: 1 });
     let lightness: number = rng.inRange({ min: 0, max: 100, step: 1 });
     let randcol_hex: string = hsluvToHex([hue, saturation, lightness]);
     return randcol_hex;
+}
+*/
+
+function createRandomColor(rng: Random): string {
+    const max_luv_samples = 100;
+    let luv_sample_failed = true;
+    let rand_color_hex:string;
+
+    //See if we can pull out a sample inside the LUV solid
+    for (let i=0; i<max_luv_samples; i++) {
+	let L = rng.inRange({ min: 0, max: 100, step: 1 });
+	let u = rng.inRange({ min: -128, max: 128, step: 1 });
+	let v = rng.inRange({ min: -128, max: 128, step: 1 });
+	let rand_color = colorspaces.make_color('CIELUV', [L, u, v]);
+	
+	if(rand_color.is_displayable()) {
+	    //console.log(`Displayable: ${L}, ${u}, ${v} on iter ${i}`);
+	    rand_color_hex = rand_color.as('hex');
+	    luv_sample_failed = false;
+	    break;
+	}
+    }
+
+    //just sample sRGB if I couldn't sample a random LUV color
+    if(luv_sample_failed) {
+	console.log("Sampling sRGB")
+	let rgb = (new Array<number>(3)).map(() => rng.inRange({ min: 0, max: 255, step: 1 }));
+	//console.log(rgb)
+	for(let i=0; i<rgb.length; i++) {
+	    rgb[i] = rgb[i]/255;
+	}
+	//console.log(rgb)
+	let rand_color = colorspaces.make_color('sRGB',rgb);
+	rand_color_hex = rand_color.as('hex');
+    }
+
+    return rand_color_hex;
 }
 
 /**
