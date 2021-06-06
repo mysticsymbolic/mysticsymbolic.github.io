a07b40f63dc4348587fa761e7c9c3291eadd02bc
Atul Varma <varmaa@gmail.com>
2021-06-06 17:41:24 -0400

    Fix redirect bug.

--- lib/use-debounced-effect.ts
+++ lib/use-debounced-effect.ts
@@ -1,18 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 /**
  * Like useEffect(), but ensures that the effect is only
  * called when the callback hasn't changed for the
- * given number of milliseconds.
+ * given number of milliseconds. It also doesn't trigger
+ * on initial mount--only when the callback *changes* from
+ * its value on initial mount.
  *
  * Note that this means that the callback itself needs
  * to be wrapped in something like `useCallback()`, or
  * else it may never be called!
  */
 export function useDebouncedEffect(ms: number, effect: React.EffectCallback) {
-  useEffect(() => {
-    const timeout = setTimeout(effect, ms);
+  // https://stackoverflow.com/a/53180013/2422398
+  const didMountRef = useRef(false);
 
-    return () => clearTimeout(timeout);
-  }, [effect, ms]);
+  useEffect(() => {
+    if (didMountRef.current) {
+      const timeout = setTimeout(effect, ms);
+
+      return () => clearTimeout(timeout);
+    } else {
+      didMountRef.current = true;
+    }
+  }, [effect, ms, didMountRef]);
 }
