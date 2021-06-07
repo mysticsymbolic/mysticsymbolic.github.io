cee6760a8ff1e01a91f5491ca059e67258e21c46
Atul Varma <varmaa@gmail.com>
2021-06-07 07:49:12 -0400

    Require that main body of creature have at least 1 attachment point.

--- lib/pages/creature-page.tsx
+++ lib/pages/creature-page.tsx
@@ -33,9 +33,18 @@ import { Page } from "../page";
 import { RandomizerWidget } from "../randomizer-widget";
 import { VocabularyWidget } from "../vocabulary-widget";
 
+/**
+ * The minimum number of attachment points that any symbol used as the main body
+ * of a creature must have.
+ */
+const MIN_ROOT_ATTACHMENT_POINTS = 1;
+
 /** Symbols that can be the "root" (i.e., main body) of a creature. */
 const ROOT_SYMBOLS = SvgVocabulary.items.filter(
-  (data) => data.meta?.always_be_nested !== true
+  (data) =>
+    data.meta?.always_be_nested !== true &&
+    Array.from(iterAttachmentPoints(data.specs || {})).length >=
+      MIN_ROOT_ATTACHMENT_POINTS
 );
 
 type AttachmentSymbolMap = {
